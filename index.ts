987d16f2cc74acbb506f3306e159934f3382e003
Tsubasa Nomura <tsunomur@microsoft.com>
2020-01-25 06:24:36 +0000

    Update

--- index.ts
+++ index.ts
@@ -14,29 +14,34 @@ const app = new App(
     await app.start(process.env.PORT || 3000);
     await app.command('/todo', async ({ command, ack, say, context }) => {
         ack();
-
+        console.log(command);
         if (!command.text) {
             await app.client.chat.postEphemeral({
                 token: context.botToken,
                 channel: command.channel_name,
                 user: command.user_id,
-                text: `Sorry <@${command.user_id}>, you aren't registered in Acme. Please post in <#${command.channel_id}> for assistance.`
+                text: `Please confirm how to instruction: /todo <ToDo title>`,
             });
         } else {
-            const [data, errors] = await ga.createGitHubIssue({ author: "tsubasaxZZZ", repositoryName: "daily-tasks", title: `${command.text}`, body: "", labelName: `${command.channel_name}` });
-            console.log(data);
-            say(`Hi <@${command.user_id}>, I added to todo! : ${command.text}`);
+            const [data, errors] = await ga.createGitHubIssue({ author: "tsubasaxZZZ", repositoryName: process.env.GITHUB_REPOSITORY_NAME || "daily-tasks", title: `${command.text}`, body: "", labelName: `${command.channel_name}` });
+            if (errors) {
+                console.log(errors);
+                say(`Error happend: ${errors}`);
+            } else {
+                console.log(data);
+                say(`Hi <@${command.user_id}>, I added to todo! : ${command.text}`);
+            }
         }
     });
 
     await app.event('app_mention', async (response) => {
+        console.log(response);
         const result = await app.client.channels.info({
             token: response.context.botToken,
             channel: response.event.channel
         });
         const channel_name = (result.channel as any).name
         console.log(channel_name);
-        console.log(response);
 
         response.say(`${response.event.text}`);
 
